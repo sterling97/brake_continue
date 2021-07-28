7b01568137cc60720aa78fd8fc90b055de06a3ef
Jose Garcia <jose_garcia0306@hotmail.com>
2021-07-27 21:58:35 -0400

    Comit para subir a github

---
+++ brake_continue.py
@@ -0,0 +1,11 @@
+def run():
+    LIMITE = 11
+    contador = 0
+    while contador < LIMITE:
+        print(contador)
+        contador += 1
+        if contador == 10:
+            break
+
+if __name__ == '__main__':
+    run()
